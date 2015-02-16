97fe384cb8c342eb7c8a788d6ca5bada2bd2272c
Janusz Dymidziuk <janusz@dymidziuk.de>
2015-02-16 19:14:41 +0100

    Initial Commit

---
+++ Umwandlung/Umwandlung/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umwandlung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
